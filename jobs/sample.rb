788eba35174bbf45f87c818707e213805a621da0
Bianca Welds <bwelds@gmail.com>
2014-04-13 10:35:19 -0400

    made a dashboard to show all of the things

---
+++ jobs/sample.rb
@@ -0,0 +1,21 @@
+current_valuation = 0
+current_karma = 0
+
+SCHEDULER.every '2s' do
+  last_valuation = current_valuation
+  last_karma     = current_karma
+  current_valuation = rand(100)
+  current_karma     = rand(200000)
+
+  send_event('valuation', { current: current_valuation, last: last_valuation })
+  send_event('karma', { current: current_karma, last: last_karma })
+  send_event('synergy',   { value: rand(10) })
+  
+end
+
+SCHEDULER.every '1s' do
+ 
+  
+  the_time = Time.now
+send_event('welcome', { title: "The amazing title", text: "The time is #{the_time}" })
+end
