e372664c97832cfdd50cd52707efca34f05e153d
Bianca Welds <bwelds@gmail.com>
2014-04-13 13:00:10 -0400

    Added widget to display Twitter faves

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -23,7 +23,27 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
       send_event('twitter_mentions', comments: tweets)
+      
     end
+
+    
+  rescue Twitter::Error
+    puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
+  end
+end
+
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  begin
+    
+    faves = twitter.favorites
+    if faves
+        faves = faves.map do |tweet|
+        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+      end
+      send_event('twitter_faves', comments: faves)
+        
+    end
+    
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
   end
